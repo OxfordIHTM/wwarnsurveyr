74b44d3acff6f9088227d664c2839abd3caee6ff
Ernest Guevarra <ernest@guevarra.io>
2020-10-03 07:02:31 +0100

    rename project

---
+++ R/wwarnsurveyr.R
@@ -0,0 +1,24 @@
+################################################################################
+#
+#'
+#' WWARN Antimalarial Resistance Surveyors in R
+#'
+#' The WorldWide Antimalarial Resistance Network (WWARN)'s mission is
+#' to generate innovative tools and reliable evidence to inform the malaria
+#' community on the factors affecting the efficacy of antimalarial medicines.
+#' WWARN works with collaborators to optimise the efficacy of antimalarial
+#' medicines and treatment regimens, especially for vulnerable groups including
+#' pregnant women, infants and malnourished children, and provides evidence to
+#' inform the development of new antimalarial drugs. WWARN also engages and
+#' works with other disease research communities to replicate this model for
+#' other tropical and neglected diseases. WWARN's interactive maps summarise
+#' data from published studies and data stored in the WWARN data centre.
+#'
+#' @docType package
+#' @keywords internal
+#' @name wwarnsurveyr
+#'
+#'
+#
+################################################################################
+"_PACKAGE"
