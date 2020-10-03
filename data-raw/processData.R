d7c3ea50384ea532c9c2d6978dd00f3b1bc93193
Ernest Guevarra <ernest@guevarra.io>
2020-10-03 06:34:05 +0100

    document datasets

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -1,5 +1,6 @@
 ## Create datasets
 library(readxl)
+library(stringi)
 
 ## ACT partner drug molecular surveyor data ####################################
 x <- read_xls(path = "data-raw/WWARN_ACT_Partner_Drug_Mol_Surveyor_data.xls",
@@ -34,6 +35,19 @@ usethis::use_data(sulphadoxine, overwrite = TRUE, compress = "xz")
 
 ## Vivax #######################################################################
 x <- read.csv(file = "data-raw/Data_and_Instructions/IDDO_NMFI_Review_data.csv")
+
+x$title <- iconv(x$title, to = "UTF-8")
+x$first_author <- iconv(x$first_author, to = "UTF-8")
+x$journal <- iconv(x$journal, to = "UTF-8")
+x$abstract_content <- iconv(x$abstract_content, to = "UTF-8")
+x$url <- iconv(x$url, to = "UTF-8")
+x$clinical_disorder_name <- iconv(x$clinical_disorder_name, to = "UTF-8")
+x$clinical_disorder.snomed_concept_name <- iconv(x$clinical_disorder.snomed_concept_name, to = "UTF-8")
+x$country_name <- iconv(x$country_name, to = "UTF-8")
+x$province_name <- iconv(x$province_name, to = "UTF-8")
+x$site_name <- iconv(x$site_name, to = "UTF-8")
+
+#stringi::stri_enc_mark(x$abstract_content)
 
 ## create new object
 vivax <- tibble::tibble(x)
